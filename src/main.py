741050b220f582117ff6e73e6ce083236b0ed51a
katsudon16 <sucipto.kathleen@gmail.com>
2019-12-24 11:07:07 -0500

    Init folder structure

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# 1. compute weights
+# 2. compute smoothness of the low-dimensional representation
+# 3. compute objective functions
+# 4. build update functions
+
+
+# first: use COIL20 dataset for testing
+if __name__ == "__main__":
