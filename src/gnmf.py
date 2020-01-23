819c523d05786491b824e4e3f0bc2d466996f142
katsudon16 <sucipto.kathleen@gmail.com>
2020-01-23 16:22:33 -0500

    Ensure weight matrix is symmetric

--- src/gnmf.py
+++ src/gnmf.py
@@ -1,7 +1,17 @@
 import numpy as np
 
 class Gnmf(object):
-    def __init__(self, method="euclidean"):
+    """
+    Graph regularized NMF
+    """
+    def __init__(self, rank=10, p=3, method="euclidean"):
+        """
+        - rank  : NMF rank
+        - p     : # closest neighbors to be taken into account in the weight matrix
+        - method: "euclidean" or "divergence"
+        """
+        self.rank = rank
+        self.p = p
         self.method = method
 
     def init_rand_matrix(self, nrow, ncol, seed=None):
@@ -13,12 +23,13 @@ class Gnmf(object):
         np.random.seed(seed)
         return(np.random.dirichlet(np.ones(nrow), size=ncol).T)
 
-    def get_weights_matrix(self, X, p=3):
+    def get_weights_matrix(self, X):
         """
         Generate weights matrix by p-nearest neighbors + dot-product weighting
         Complexity: O(N^2 * M)
         """
         m, n = X.shape
+        p = self.p
         # initialize distance matrix and W matrix
         dist_matrix = np.full((n, n), np.inf)
         W = np.zeros((n, n))
@@ -33,7 +44,8 @@ class Gnmf(object):
             for j in range(p):
                 neighbor = sorted_idx[i][j]
                 # compute dot-product weighting
-                W[i][neighbor] = np.dot(X[:,i], X[:,neighbor])
+                #TODO: ensure W is symmetric?
+                W[i][neighbor] = W[neighbor][i] = np.dot(X[:,i], X[:,neighbor])
         return(W)
 
     def update_euclidean(self, X, U, V, W, lmbda=100):
@@ -49,15 +61,16 @@ class Gnmf(object):
         U = U * np.divide(X @ V.T, U @ V @ V.T)
         # calc objective func
         R = X - (U @ V)
-        D = np.sum(R * R) + np.trace(lmbda * (V @ L @ V.T))
+        D = np.sum(R * R) + lmbda * np.trace(V @ L @ V.T)
         return(U, V, D)
 
-    def factorize(self, X, rank=10, n_iter = 100, method="euclidean"):
+    def factorize(self, X, n_iter=100, method="euclidean"):
         """
         Factorize matrix X into W and H given rank using multiplicative method
         method options: ["euclidean", "divergence"]
         """
         n, m = X.shape
+        rank = self.rank
         print("generating weight matrix...")
         W = self.get_weights_matrix(X)
         U = self.init_rand_matrix(n, rank)
@@ -66,4 +79,8 @@ class Gnmf(object):
         for iter in range(n_iter):
             U, V, obj_val = self.update_euclidean(X, U, V, W)
             print("Iteration %d; objective value = %.2f" % (iter, obj_val))
+        # set the euclidean length of each col vec in U = 1
+        sum_col_U = np.sqrt(np.sum(U**2, axis=0))
+        U = U / sum_col_U
+        V = V / sum_col_U.reshape((rank, 1))
         return(U, V)
